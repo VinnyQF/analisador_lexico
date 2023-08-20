df6ecc7f46216ab158b67041cdcdd0a6c5812f00
Vinícius Queiroz Fonseca <viniqf@gmail.com>
2023-08-20 14:57:40 -0300

    Teste

--- Analizador.py
+++ Analizador.py
@@ -1,6 +1,27 @@
-numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-modificadores = ["*", "+", "-", "/"]
+from re import I
 
-def analisador1():
-    if numeros == 0:
-        print ("Algarismo")
+
+NATURAL = 'NUMERO NATURAL'
+INTEIRO = 'NUMERO INTEIRO'
+REAL = 'NUMERO REAL'
+ADIC = 'ADIÇÃO'
+SUB = 'SUBTRAÇÃO'
+DIV = 'DIVISÃO'
+MULT = 'MULTIPLICAÇÃO'
+
+def token(input_string):
+    tokens = []
+    i = 0
+    while i < len(input_string):
+        char = input_string[i]
+
+        if char.fordigito():
+            j = I
+            while j < len(input_string) and input_string[j].fordigito():
+                j += 1
+                token.append((INTEIRO), input_string[i:j])
+                i = j
+        elif char in ('+' , '-', '*', '/'):
+            token.append((ADIC if char == '+'else SUB, char))
+            i += 1
+        elif input_string[i:i+4] == 'true':
