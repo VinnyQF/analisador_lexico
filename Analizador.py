215389d9e112301cee5673407c31fd73517cc3f3
Vinícius Queiroz Fonseca <viniqf@gmail.com>
2023-08-20 14:55:28 -0300

    Modificação de teste para obter novo resultado

--- Analizador.py
+++ Analizador.py
@@ -2,5 +2,5 @@ numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 modificadores = ["*", "+", "-", "/"]
 
 def analisador1():
-    if numeros: 1
-    print ("true")
+    if numeros == 0:
+        print ("Algarismo")
